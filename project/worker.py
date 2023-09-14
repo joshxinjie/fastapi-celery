701a2053bd0e04b389cf6290d8ccbf3d39a84bf2
xinjielee <xinjielee@aisingapore.org>
2023-09-14 13:51:46 +0800

    update flower broker url

--- project/worker.py
+++ project/worker.py
@@ -6,7 +6,7 @@ from celery import Celery
 
 celery = Celery(__name__)
 # celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "pyamqp://admin:mypass@localhost:5672")
+celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://admin:mypass@localhost:5672")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
 # celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "amqp://admin:mypass@localhost:5672")
 
